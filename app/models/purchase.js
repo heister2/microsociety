f52b9b7ba6872c4350f89938857e5e97a9656767
heister2 <heister2@users.noreply.github.com>
2015-02-17 13:35:20 -0600

    Update purchase.js

--- app/models/purchase.js
+++ app/models/purchase.js
@@ -4,7 +4,8 @@ var purchaseSchema = mongoose.Schema({
   item: { type: String, required: true },
   quantity: { type: Number, required: true },
   price: { type: Number, required: true },
-  total: { type: Number, required: true }
+  total: { type: Number, required: true },
+  cashier: { type: Number}
 });
 
 module.exports = mongoose.model('Purchase', purchaseSchema);
